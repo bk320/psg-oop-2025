3cc466f6a051c6effe0ae138e40c8121dab252aa
Richard Choquerive Ramos <kurivavilo@gmail.com>
2025-10-25 16:58:14 -0400

    Sesión 06

---
+++ sesion06/composicion.py
@@ -0,0 +1,17 @@
+# Definición
+class Corazon:
+    def __init__(self, peso):
+        self.peso = peso
+
+class Cuerpo:
+    def __init__(self, nombre, corazon_peso):
+        self.nombre = nombre
+        self.corazon = Corazon(corazon_peso)  # Composición
+    
+    def ver(self):
+        print(f"Cuerpo: {self.nombre}")
+        print(f"Corazón: Peso {self.corazon.peso} kg")
+
+# Uso
+cuerpo = Cuerpo("Humano", 0.3)
+cuerpo.ver()
